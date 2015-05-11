b960d3cba3f709ffbdeb4c32f0b9f6ee005a4df6
gerardodelapiedra <gerardo.delapiedra@gmail.com>
2015-05-10 21:43:39 -0500

    Finished the RCAV practice!
    Completed

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -3,8 +3,29 @@ class CalculationsController < ApplicationController
   end
 
   def sqrt
+
+    @number = params[:number]
+    @sqrt = Math.sqrt(@number.to_i).round(2)
+
   end
 
-  def pmt
+  def sqr
+
+    @number = params[:number]
+    @sqr = @number.to_i**2
+
+  end
+
+
+  def payment
+
+    @interest = params[:i].to_f
+    @period = params[:m].to_i
+    @principal = params[:principal].to_f
+
+    @payment = (@interest/100 * @principal)/(1 - (1 + @interest/100)**-@period).round(2)
+    @pmt = @payment.round(2)
+
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square_root/:number", { :controller => "calculations", :action => "sqrt" })
+  get("/square/:number", { :controller => "calculations", :action => "sqr" })
+  get("/payment/:i/:m/:principal", { :controller => "calculations", :action => "payment" })
+
+
+
 end
